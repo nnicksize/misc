18284a434e4706ca4210b1cefb77b3607d4a8618
Dominik Hirzel <hirzeldominik@gmail.com>
2017-06-16 10:27:51 +0200

    Add deploy file for vs2017

---
+++ snippets/Visual C#/vs2017_deploy.ps1
@@ -0,0 +1,3 @@
+$srcPath = Split-Path -parent $PSCommandPath
+$destPath = Join-Path (gc env:userprofile) "\Documents\Visual Studio 2017\Code Snippets\Visual C#\My Code Snippets\"
+Get-ChildItem $srcPath -Filter "*.snippet" | Copy-Item -Destination $destPath
